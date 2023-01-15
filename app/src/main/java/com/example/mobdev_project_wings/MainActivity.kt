6f4059f96e82666061f3eb9f8c42fb68b8b457c7
Laith Abusada <laithps4@hotmail.com>
2023-01-15 19:55:39 +0300

    MAP IS NOW WORKING

--- app/src/main/java/com/example/mobdev_project_wings/MainActivity.kt
+++ app/src/main/java/com/example/mobdev_project_wings/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.mobdev_project_wings
 import android.content.ContentProvider
 import android.content.ContentValues
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
@@ -13,12 +14,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val order:Button= findViewById(R.id.orderBtn)
-
+        val location:Button = findViewById(R.id.locationBtn);
         order.setOnClickListener{
             val intent= Intent(this, SecondActivity::class.java)
             startActivity(intent)
         }
-        
+        location.setOnClickListener() {
+            val mapIntent: Intent = Uri.parse(
+                "https://goo.gl/maps/CRgKSqhb4hdn3jeWA"
+            ).let { location ->
+                // Or map point based on latitude/longitude
+                Intent(Intent.ACTION_VIEW, location)
+            }
+            startActivity(mapIntent);
+        }
     }
 
 }
